02ca1d5b877924aaa1047bbbff6c9fed0ab5a93f
Moony <42318815+DevMoony@users.noreply.github.com>
2024-12-13 11:09:52 +0100

    Update index.go

--- index.go
+++ index.go
@@ -1,14 +1,29 @@
 package store
 
 import (
-	"github.com/GalacticDocs/store-go/cache"
-	"github.com/GalacticDocs/store-go/set"
+	cache_storage "github.com/GalacticDocs/store-go/cache"
+	set_storage "github.com/GalacticDocs/store-go/set"
+	map_storage "github.com/GalacticDocs/store-go/map"
+	collection_storage "github.com/GalacticDocs/store-go/collection"
+	weakset_storage "github.com/GalacticDocs/store-go/weakset"
 )
 
 func Cache[T any](capacity int) *cache.Cache[T] {
-	return cache.New[T](capacity)
+	return cache_storage.New[T](capacity)
+}
+
+func WeakSet[T comparable]() *weakset_storage.WeakSet[T] {
+	return weakset_storage.New[T]()
 }
 
 func Set[T comparable]() *set.Set[T] {
-	return set.New[T]()
+	return set_storage.New[T]()
+}
+
+func Map() *map_storage.IMap {
+	return map_storage.Map()
+}
+
+func Collection() *collection_storage {
+	return collection_storage.New()
 }
